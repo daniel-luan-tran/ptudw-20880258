b46419607938e0876d3a1a36aa824b4d8481ea24
luan.tran <luan.tran@blackbook.ai>
2023-05-13 23:59:24 +0700

    Danh sách sản phẩm theo Brand

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,24 +4,37 @@ const controller = {};
 const models = require('../models');
 
 controller.show = async (req, res) => {
-    let categoryId = parseInt(req.query.category) || 0;
-
+    let categoryId = parseInt(req.query.categoryId) || 0;
+    let brandId = parseInt(req.query.brandId) || 0;
+    let brands = await models.Brand.findAll({
+        include: [{
+            model: models.Product
+        }]
+    });
+    
     let categories = await models.Category.findAll({
         include: [{
             model: models.Product
         }]
     });
 
-    let products = await models.Product.findAll({
+    let productsOption = {
         attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice', 'categoryId'],
-        where: categoryId != 0 ? {categoryId: categoryId} : {},
+        where: {},
         include: [{
             model: models.Category,
             attributes: ['name']
         }]
-    });
+    }
+
+    if (categoryId != 0) productsOption.where.categoryId = categoryId;
+    
+    if (brandId!= 0) productsOption.where.brandId = brandId;
+
+    let products = await models.Product.findAll(productsOption);
 
     res.locals.categories = categories;
+    res.locals.brands = brands;
     console.log(categories);
     res.locals.products = products;
     console.log('render product-list');
