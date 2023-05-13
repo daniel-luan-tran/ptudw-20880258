eefa19828584326c0a1151b9cec05957cba00599
luan.tran <luan.tran@blackbook.ai>
2023-05-13 23:31:52 +0700

    Danh sách sản phẩm theo Category

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,10 +4,25 @@ const controller = {};
 const models = require('../models');
 
 controller.show = async (req, res) => {
-    let products = await models.Product.findAll({
-        attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice']
+    let categoryId = parseInt(req.query.category) || 0;
+
+    let categories = await models.Category.findAll({
+        include: [{
+            model: models.Product
+        }]
     });
 
+    let products = await models.Product.findAll({
+        attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice', 'categoryId'],
+        where: categoryId != 0 ? {categoryId: categoryId} : {},
+        include: [{
+            model: models.Category,
+            attributes: ['name']
+        }]
+    });
+
+    res.locals.categories = categories;
+    console.log(categories);
     res.locals.products = products;
     console.log('render product-list');
     res.render('product-list');
