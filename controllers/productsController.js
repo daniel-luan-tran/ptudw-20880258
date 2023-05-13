ef594e226372b15e0688fb90d7013f7aa3992942
luan.tran <luan.tran@blackbook.ai>
2023-05-14 00:15:34 +0700

    Danh sách sản phẩm theo Tag

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,6 +6,14 @@ const models = require('../models');
 controller.show = async (req, res) => {
     let categoryId = parseInt(req.query.categoryId) || 0;
     let brandId = parseInt(req.query.brandId) || 0;
+    let tagId = parseInt(req.query.tagId) || 0;
+
+    let tags = await models.Tag.findAll({
+        include: [{
+            model: models.Product
+        }]
+    });
+
     let brands = await models.Brand.findAll({
         include: [{
             model: models.Product
@@ -21,22 +29,27 @@ controller.show = async (req, res) => {
     let productsOption = {
         attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice', 'categoryId'],
         where: {},
-        include: [{
-            model: models.Category,
-            attributes: ['name']
-        }]
+        include: []
     }
 
     if (categoryId != 0) productsOption.where.categoryId = categoryId;
     
     if (brandId!= 0) productsOption.where.brandId = brandId;
+    
+    if (tagId!= 0) {
+        productsOption.include = [{
+            model: models.Tag,
+            where: {id: tagId}
+        }]
+    }
 
     let products = await models.Product.findAll(productsOption);
 
     res.locals.categories = categories;
     res.locals.brands = brands;
-    console.log(categories);
+    res.locals.tags = tags;
     res.locals.products = products;
+
     console.log('render product-list');
     res.render('product-list');
 };
