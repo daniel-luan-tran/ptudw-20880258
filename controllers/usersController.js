01b0caa6b1f6cf09092cefdc65d37acd8bf208b3
luan.tran <luan.tran@blackbook.ai>
2023-05-20 20:01:35 +0700

    Update: Chuc nang checkout

---
+++ controllers/usersController.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const controller = {};
+const models = require('../models');
+
+controller.checkout = async (req, res) => {
+    if (req.session.cart.quantity > 0) {
+        let userId = 1;
+        res.locals.address = await models.Address.findAll({
+            where : {
+                userId
+            }
+        })
+        res.locals.cart = req.session.cart.getCart();
+        return res.render('checkout');
+    }
+    res.redirect('/products');
+}
+
+module.exports = controller;
